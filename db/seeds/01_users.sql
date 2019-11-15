15f37b5ac547c70c04962d38a96ce81b08d38ac7
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-15 20:38:08 +0000

    users format

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,9 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users
+  (name)
+VALUES
+  ('Alice');
+INSERT INTO users
+  (name)
+VALUES
+  ('Kira');
