a51bf674a837e7b3c9734ff13e7348bb869718d5
Sam <samuelrush@gmail.com>
2019-11-15 20:36:11 +0000

    added 3 fake users

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,21 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users
+  (name)
+VALUES
+  ('Alice');
+INSERT INTO users
+  (name)
+VALUES
+  ('Kira');
+INSERT INTO users
+  (name)
+VALUES
+  ('Person1');
+INSERT INTO users
+  (name)
+VALUES
+  ('Person2');
+INSERT INTO users
+  (name)
+VALUES
+  ('Person3');
