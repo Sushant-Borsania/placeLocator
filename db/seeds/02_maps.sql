7efa4fbeb921d16b0d3912363dc6cc1de0d94798
Sam <samuelrush@gmail.com>
2019-11-16 03:55:59 +0000

    Prepare db Schema and some seeds

---
+++ db/seeds/02_maps.sql
@@ -0,0 +1,14 @@
+INSERT INTO maps
+  (contributor_id,name,category,owner_id)
+VALUES
+  (1, 'FoodMap', 'Food', 1);
+
+INSERT INTO maps
+  (contributor_id,name,category,owner_id)
+VALUES
+  (2, 'GymMap', 'Gym', 2);
+
+INSERT INTO maps
+  (contributor_id,name,category,owner_id)
+VALUES
+  (1, 'MallMap', 'Shopping', 2);
