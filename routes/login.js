d3619c8b06f100c8a12126c4627429bbb98b81c6
Sam <samuelrush@gmail.com>
2019-11-19 06:55:48 +0000

    Post request in login js

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,41 @@ const router = express.Router();
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    res.render("login");
+    if (req.session === undefined) {
+      res.render("login");
+    } else {
+      res.redirect(`/`);
+    }
   });
+};
+
+module.exports = db => {
+  router
+    .route("/")
+    .get((req, res) => {
+      if (req.session === undefined) {
+        res.render("login");
+      } else {
+        res.redirect(`/`);
+      }
+    })
+    .post((req, res) => {
+      // console.log("username", req.body.usernameID);
+      // console.log("password", req.body.passwordID);
+      db.query(`SELECT * FROM users;`).then(data => {
+        const users = data.rows;
+        for (let user of users) {
+          if (user.username === req.body.usernameID) {
+            if (user.password === req.body.passwordID) {
+              //store cookie and redirect
+              res.redirect(`/`);
+            } else {
+              res.send("Password is incorrect");
+            }
+          }
+          //res.send("Username does not exist!"); //does not work
+        }
+      });
+    });
   return router;
 };
