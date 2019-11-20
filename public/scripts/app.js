ffa96ea0d1cf5a5327e551f7b337737905e9e43a
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-20 04:16:59 +0000

    Link map with their Ids

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -45,3 +45,20 @@ const mapCreator = data => {
     }
   });
 };
+
+//Create function that will create map based on ID of map
+
+const createSingleMap = data => {
+  let mymap = L.map("singleMap").setView(data.latLang, 14);
+  L.tileLayer(
+    "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
+    {
+      maxZoom: 18,
+      id: "mapbox.streets"
+    }
+  ).addTo(mymap);
+  data.flagCords.forEach(flag => {
+    L.marker(flag).addTo(mymap);
+  });
+  mymap.fitBounds(data.flagCords);
+};
--- routes/maps.js
+++ routes/maps.js
@@ -3,33 +3,58 @@ const router = express.Router();
 
 module.exports = db => {
   router.get("/", (req, res) => {
-    if (req.session.user_id2 !== undefined) {
-      let query = `SELECT maps.id
-      FROM maps
-        JOIN users ON maps.owner_id = users.id
-      WHERE maps.owner_id = (SELECT users.id
-      FROM users
-      WHERE users.username = '${req.session.user_id2}');
-      `;
-      db.query(query).then(data => {
-        let params = req.map_config;
-        const maps = data.rows;
-        let mapOwner = false;
-        for (let map of maps) {
-          if (Number(map.id) === Number(params.map_id)) {
-            mapOwner = true;
-          }
+    let params = req.map_config;
+    let query2 = `select maps.*, flags.id as flag_id, flags.latlong, flags.address, flags.map_id, flags.title, flags.description, flags.image, flags.user_id as flag_user_id from maps LEFT join flags on maps.id = flags.map_id where maps.id = ${Number(
+      params.map_id
+    )};`;
+    db.query(query2).then(data => {
+      // console.log("IDD",data.rows[0]);
+      let latLong = Object.values(data.rows[0].map_latlong);
+      //flag stuff started here
+      let flagCords = [];
+      data.rows.forEach(el => {
+        if (el.latlong !== null) {
+          flagCords.push(Object.values(el.latlong));
         }
+      });
+
+      //Adding %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+      if (req.session.user_id2 !== undefined) {
+        let query = `SELECT maps.id
+        FROM maps
+          JOIN users ON maps.owner_id = users.id
+        WHERE maps.owner_id = (SELECT users.id
+        FROM users
+        WHERE users.username = '${req.session.user_id2}');
+        `;
+        db.query(query).then(data => {
+          let params = req.map_config;
+          const maps = data.rows;
+          let mapOwner = false;
+          for (let map of maps) {
+            if (Number(map.id) === Number(params.map_id)) {
+              mapOwner = true;
+            }
+          }
+          let templateVars = {
+            user_id: req.session["user_id2"],
+            owner: mapOwner,
+            mapDetails: latLong,
+            flagCords: JSON.stringify(flagCords)
+          };
+          res.render("maps", templateVars);
+        });
+      } else {
         let templateVars = {
           user_id: req.session["user_id2"],
-          owner: mapOwner
+          mapDetails: latLong,
+          flagCords: JSON.stringify(flagCords)
         };
         res.render("maps", templateVars);
-      });
-    } else {
-      let templateVars = { user_id: req.session["user_id2"] };
-      res.render("maps", templateVars);
-    }
+      }
+
+      //Closed%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    });
   });
   return router;
 };
