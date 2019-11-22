152c18517af0ba9a93c9a7828d042e2b716e3a2d
Sushant-ABdigital <borsaniasushant@gmail.com>
2019-11-22 07:49:31 +0000

    Add responsive design

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,7 +25,7 @@ const mapCreator = data => {
     L.tileLayer(
       "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw",
       {
-        maxZoom: 18,
+        maxZoom: 14,
         id: "mapbox.streets"
       }
     ).addTo(mymap);
